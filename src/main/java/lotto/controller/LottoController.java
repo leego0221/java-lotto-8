1d3de352487168372f892495726c3046bbfd23e9
leego0221 <leego0221@naver.com>
2025-10-29 23:01:35 +0900

    feat: 구입 금액 입력

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,21 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        String purchaseAmountInput = inputView.readPurchaseAmount();
+
+        System.out.println("purchaseAmountInput = " + purchaseAmountInput);
+    }
+}
