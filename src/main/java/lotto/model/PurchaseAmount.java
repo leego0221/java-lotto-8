e66b43bcff54f9d093dc46fdc0f710e3f083f26d
leego0221 <leego0221@naver.com>
2025-10-30 00:07:55 +0900

    test: PurchaseAmount 객체 생성 시 값 검증 테스트 작성

---
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -0,0 +1,10 @@
+package lotto.model;
+
+public class PurchaseAmount {
+
+    private final int purchaseAmount;
+
+    public PurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+}
---
+++ src/test/java/lotto/model/PurchaseAmountTest.java
@@ -0,0 +1,44 @@
+package lotto.model;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PurchaseAmountTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @ParameterizedTest
+    @ValueSource(ints = {999, 0, -3, -1000})
+    void 구입_금액이_1000원_미만이면_예외가_발생한다(int input) {
+        // given by parameter
+
+        // when & then
+        assertThatThrownBy(() -> new PurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 3000, 5000})
+    void 구입_금액이_1000원_단위이면_테스트에_성공한다(int input) {
+        // given by parameter
+
+        // when & then
+        assertThatCode(() -> new PurchaseAmount(input))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1500, 3500, 5500})
+    void 구입_금액이_1000원_단위가_아니면_예외가_발생한다(int input) {
+        // given by parameter
+
+        // when & then
+        assertThatThrownBy(() -> new PurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+}
