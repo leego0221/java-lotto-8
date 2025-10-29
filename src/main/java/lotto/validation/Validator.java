fef6f7f9a0fe2c446cc2ee531486ca4d9d40a078
leego0221 <leego0221@naver.com>
2025-10-29 22:21:44 +0900

    test: 보너스 번호 중복 검증 테스트 작성

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -1,5 +1,7 @@
 package lotto.validation;
 
+import lotto.model.WinningNumbers;
+
 public class Validator {
 
     private static final int LOTTO_PRICE = 1000;
@@ -8,5 +10,8 @@ public class Validator {
         if (purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
         }
+    }
+
+    public void validateDuplicate(WinningNumbers winningNumbers, int bonusNumber) {
     }
 }
--- src/test/java/lotto/validation/ValidatorTest.java
+++ src/test/java/lotto/validation/ValidatorTest.java
@@ -1,6 +1,9 @@
 package lotto.validation;
 
+import lotto.model.WinningNumbers;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -8,11 +11,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ValidatorTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
+    private final Validator validator = new Validator();
 
     @Test
     void 구입_금액이_1000원_단위이면_테스트에_성공한다() {
         // given
-        Validator validator = new Validator();
         int purchaseAmount = 3000;
 
         // when & then
@@ -23,11 +26,33 @@ class ValidatorTest {
     @Test
     void 구입_금액이_1000원_단위가_아니면_예외가_발생한다() {
         // given
-        Validator validator = new Validator();
         int purchaseAmount = 3500;
 
         // when & then
         assertThatThrownBy(() -> validator.validatePurchaseAmount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    void 보너스_번호가_당첨_번호들과_중복되지_않으면_테스트에_성공한다() {
+        // given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        // when & then
+        assertThatCode(() -> validator.validateDuplicate(winningNumbers, bonusNumber))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 보너스_번호가_당첨_번호들과_중복되면_예외가_발생한다() {
+        // given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 3;
+
+        // when & then
+        assertThatThrownBy(() -> validator.validateDuplicate(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
     }
