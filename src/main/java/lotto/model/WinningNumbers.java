2d8a9578f7dde673c73fdcb49ed94d1e1fda0eae
leego0221 <leego0221@naver.com>
2025-10-29 22:00:35 +0900

    feat: 당첨 번호 입력값 검증

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -7,6 +7,17 @@ public class WinningNumbers {
     private final List<Integer> winningNumbers;
 
     public WinningNumbers(List<Integer> winningNumbers) {
+        validate(winningNumbers);
         this.winningNumbers = winningNumbers;
+    }
+
+    private void validate(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+
+        if (winningNumbers.size() != winningNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 중 중복된 숫자가 있습니다.");
+        }
     }
 }
--- src/main/java/lotto/parser/WinningNumberParser.java
+++ src/main/java/lotto/parser/WinningNumberParser.java
@@ -1,10 +1,15 @@
 package lotto.parser;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class WinningNumberParser {
 
     public static List<Integer> parse(String input) {
-        return List.of();
+        String[] output = input.split(",", -1);
+
+        return Arrays.stream(output)
+                .map(Integer::parseInt)
+                .toList();
     }
 }
