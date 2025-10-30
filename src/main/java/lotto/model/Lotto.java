e430499da7c8849dab67a11692b9c68813ca8dfd
leego0221 <leego0221@naver.com>
2025-10-30 21:12:31 +0900

    feat: 로또 생성 시 중복 검증

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -26,6 +26,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 중 중복된 숫자가 있습니다.");
+        }
     }
 
     private boolean isWinningNumber(WinningNumbers winningNumbers, Integer number) {
