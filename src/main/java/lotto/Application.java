bd06ae61c5db23b5c9964117b3777155dbba6d8b
leego0221 <leego0221@naver.com>
2025-10-31 22:53:49 +0900

    refactor: AppConfig 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,21 +1,13 @@
 package lotto;
 
+import lotto.config.AppConfig;
 import lotto.controller.LottoController;
-import lotto.domain.LottoRankCounter;
-import lotto.service.LottoService;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-
-        LottoRankCounter lottoRankCounter = new LottoRankCounter();
-        LottoService lottoService = new LottoService(lottoRankCounter);
-
-        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+        AppConfig appConfig = new AppConfig();
+        LottoController lottoController = appConfig.lottoController();
 
         lottoController.run();
     }
---
+++ src/main/java/lotto/config/AppConfig.java
@@ -0,0 +1,30 @@
+package lotto.config;
+
+import lotto.controller.LottoController;
+import lotto.domain.LottoRankCounter;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class AppConfig {
+
+    public LottoController lottoController() {
+        return new LottoController(inputView(), outputView(), lottoService());
+    }
+
+    public InputView inputView() {
+        return new InputView();
+    }
+
+    public OutputView outputView() {
+        return new OutputView();
+    }
+
+    public LottoService lottoService() {
+        return new LottoService(lottoRankCounter());
+    }
+
+    public LottoRankCounter lottoRankCounter() {
+        return new LottoRankCounter();
+    }
+}
