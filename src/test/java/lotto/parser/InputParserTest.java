2ab577ad3d6870c7de28ad7d01fa363a7874b659
leego0221 <leego0221@naver.com>
2025-11-01 03:03:34 +0900

    test: InputParserTest 보완

--- src/test/java/lotto/parser/InputParserTest.java
+++ src/test/java/lotto/parser/InputParserTest.java
@@ -1,22 +1,61 @@
 package lotto.parser;
 
+import lotto.exception.ErrorCode;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputParserTest {
 
     @Test
-    void 당첨_번호가_쉼표_기준으로_구분되면_테스트에_성공한다() {
+    void 쉼표_기준으로_구분되고_각_요소가_정수면_테스트에_성공한다() {
         // given
         String input = "1,2,3,4,5,6";
 
         // when
-        List<Integer> result = InputParser.parseWinningNumbers(input);
+        List<Integer> winningNumbers = InputParser.parseWinningNumbers(input);
 
         // then
-        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6);
+        assertThat(winningNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 쉼표_기준으로_구분되고_각_요소가_앞뒤_공백이_있어도_테스트에_성공한다() {
+        // given
+        String input = " 1,  2,  3 , 4  ,5  ,6 ";
+
+        // when
+        List<Integer> winningNumbers = InputParser.parseWinningNumbers(input);
+
+        // then
+        assertThat(winningNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,", "1,,3", ",2,3", "1,2, ", "1, ,3", " ,2,3"})
+    void 쉼표_기준으로_구분되어도_각_요소가_공백_계열이면_테스트에_실패한다(String input) {
+        // given by parameter
+
+        // when & then
+        assertThatThrownBy(() -> InputParser.parseWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.ERROR_PARSED_BLANK.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a,2,3", "1,a,3", "1,2,a"})
+    void 쉼표_기준으로_구분되어도_각_요소가_정수가_아니면_테스트에_실패한다(String input) {
+        // given by parameter
+
+        // when & then
+        assertThatThrownBy(() -> InputParser.parseWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.ERROR_PARSED_NOT_INTEGER.getMessage());
     }
 }
