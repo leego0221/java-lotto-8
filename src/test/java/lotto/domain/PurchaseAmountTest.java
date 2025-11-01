c875a5ce5a667a7cd0f7038e14d76cf1d3bdb3e3
leego0221 <leego0221@naver.com>
2025-11-01 13:11:32 +0900

    test: PurchaseAmountTest 보완

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.exception.ErrorCode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -7,8 +8,6 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseAmountTest {
-
-    private static final String ERROR_MESSAGE = "[ERROR]";
 
     @ParameterizedTest
     @ValueSource(ints = {999, 0, -3, -1000})
@@ -18,17 +17,7 @@ class PurchaseAmountTest {
         // when & then
         assertThatThrownBy(() -> new PurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1000, 3000, 5000})
-    void 구입_금액이_1000원_단위이면_테스트에_성공한다(int input) {
-        // given by parameter
-
-        // when & then
-        assertThatCode(() -> new PurchaseAmount(input))
-                .doesNotThrowAnyException();
+                .hasMessage(ErrorCode.ERROR_AMOUNT_NOT_ENOUGH.getMessage());
     }
 
     @ParameterizedTest
@@ -39,6 +28,16 @@ class PurchaseAmountTest {
         // when & then
         assertThatThrownBy(() -> new PurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
+                .hasMessage(ErrorCode.ERROR_AMOUNT_INVALID_UNIT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 3000, 5000})
+    void 구입_금액이_1000원_단위이면_테스트에_성공한다(int input) {
+        // given by parameter
+
+        // when & then
+        assertThatCode(() -> new PurchaseAmount(input))
+                .doesNotThrowAnyException();
     }
 }
