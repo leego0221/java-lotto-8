1aeec934b7f46f5c9aaa9212baeb384ce713118c
leego0221 <leego0221@naver.com>
2025-11-01 15:28:28 +0900

    test: LottoServiceTest 보완

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,23 +1,31 @@
 package lotto.service;
 
 import lotto.domain.*;
+import lotto.exception.ErrorCode;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
 class LottoServiceTest {
 
-    private static final String ERROR_MESSAGE = "[ERROR]";
     private final LottoService lottoService = new LottoService();
 
     @Test
-    void 보너스_번호가_당첨_번호들과_중복되지_않으면_테스트에_성공한다() {
+    void 구입_금액만큼_로또를_발행하면_테스트가_성공한다() {
+        // given
+        PurchaseAmount purchaseAmount = new PurchaseAmount(3000);
+
+        // when
+        List<Lotto> lottos = lottoService.purchase(purchaseAmount);
+
+        // then
+        assertThat(lottos).hasSize(3);
+    }
+
+    @Test
+    void 보너스_번호가_당첨_번호들과_중복되지_않으면_테스트가_성공한다() {
         // given
         BonusNumber bonusNumber = new BonusNumber(7);
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
@@ -36,18 +44,6 @@ class LottoServiceTest {
         // when & then
         assertThatThrownBy(() -> lottoService.checkDuplicate(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE);
-    }
-
-    @Test
-    void 구입_금액에_해당하는_만큼_로또를_발행한다() {
-        // given
-        PurchaseAmount purchaseAmount = new PurchaseAmount(3000);
-
-        // when
-        List<Lotto> lottos = lottoService.purchase(purchaseAmount);
-
-        // then
-        assertThat(lottos).hasSize(3);
+                .hasMessageContaining(ErrorCode.ERROR_NUMBERS_AND_BONUS_DUPLICATE.getMessage());
     }
 }
